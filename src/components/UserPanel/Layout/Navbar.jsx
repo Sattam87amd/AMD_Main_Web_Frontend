6d1cfc24bd9cb9e72192c7f88277aaf201070040
Resource-04 <resource4.code4bharat@gmail.com>
2025-04-16 17:01:16 +0530

    Import google Translate

--- src/components/UserPanel/Layout/Navbar.jsx
+++ src/components/UserPanel/Layout/Navbar.jsx
@@ -6,7 +6,6 @@ import { Gift, Menu, X, Search, User } from "lucide-react";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import SeacthExperts from "@/components/SearchExperts/SearchExperts";
-import GoogleTranslate from "@/components/GoogleTranslate";
 
 
 function Navbar() {
