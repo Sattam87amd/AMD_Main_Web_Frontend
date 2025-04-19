03ceaf4a78198518206d0cb03823908696b0cec2
Resource-01 <resource1.code4bharat@gmail.com>
2025-04-19 17:07:31 +0530

    - Added SAR currancy in after see all button pages of Expert Panel.
    - Made changes in Expert Panel: See all button that redirected pages now SAR currancy is visible.
    
    - On Expert Panel: Now on after see all pages have also funtionality that user can see charity is enable or not!

--- src/components/ExpertPanel/ExpertsList/Style&BeautyExpert.jsx
+++ src/components/ExpertPanel/ExpertsList/Style&BeautyExpert.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { HiBadgeCheck } from "react-icons/hi";
-import { HiChevronRight } from "react-icons/hi";
+import { HeartHandshake } from "lucide-react";
 import axios from "axios";
 import { motion } from "framer-motion";
 
@@ -106,16 +106,29 @@ const FashionBeautyLogin = () => {
 
                 {/* Price Badge */}
                 <div className="absolute top-4 right-4 bg-[#F8F7F3] text-black px-4 py-2 rounded-2xl shadow-xl font-semibold">
-                  {expert.price || "$ 0"}  {/* Default value in case price is missing */}
+                 SAR {expert.price || "$ 0"}  {/* Default value in case price is missing */}
                 </div>
 
-                {/* Info Box */}
-                <div className="absolute bottom-1 left-1 right-1 bg-white/80 backdrop-blur-md p-4 m-2  shadow-lg">
-                  <h2 className="text-lg font-semibold text-black flex items-center gap-1">
-                    {expert.firstName}
-                    <HiBadgeCheck className="w-5 h-5 text-yellow-500" />
-                  </h2>
-                  <p className="text-xs text-gray-800 mt-1">
+                {/* Transparent Blur Card */}
+                <div className="absolute bottom-1 left-1 right-1 bg-white/80 p-4 m-2">
+                  <div className="flex items-center justify-between">
+                    <h2 className="text-lg font-semibold text-black flex items-center gap-1">
+                      {expert.firstName}
+                      <HiBadgeCheck className="w-6 h-6 text-yellow-500" />
+                    </h2>
+
+                    {/* Small charity indicator text (optional) */}
+                    {expert.charityEnabled && (
+                      <div className="flex items-center text-xs text-red-600 font-bold px-3 py-1.5 rounded-full">
+                        <span>
+                          {expert.charityPercentage || 0}% to Charity{" "}
+                        </span>
+                        <HeartHandshake className="w-3 h-3 ml-1" />
+                      </div>
+                    )}
+                  </div>
+
+                  <p className="text-xs text-black mt-1 line-clamp-3">
                     {truncateExperience(expert.experience)}
                   </p>
                 </div>
