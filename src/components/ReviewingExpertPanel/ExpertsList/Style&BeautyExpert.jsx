4e6b1130ee944522e212d64be07b0e273da39dc8
Resource-02 <resource2.code4bharat@gmail.com>
2025-05-09 10:58:36 +0530

    `Refactor expert category and style & beauty expert pages to use dynamic routes and base route`

--- src/components/ReviewingExpertPanel/ExpertsList/Style&BeautyExpert.jsx
+++ src/components/ReviewingExpertPanel/ExpertsList/Style&BeautyExpert.jsx
@@ -95,14 +95,14 @@ const FashionBeautyLogin = () => {
         transition={{ duration: 0.6 }}
         className="flex justify-between items-center mb-6"
       >
-        <div className="flex flex-col md:flex-row md:items-center">
-          <h1 className="text-3xl md:text-[60px] font-bold text-black">
-            FASHION & BEAUTY
-          </h1>
-          <p className="text-[#9C9C9C] md:pt-5 pl-5 md:text-2xl">
-            Access to the best experts has never been easier
-          </p>
-        </div>
+        <div className="flex flex-col md:flex-row md:h-40 items-center mb-6 md:mb-0">
+        <h1 className="text-5xl md:text-[60px] font-bold text-black">
+          FASHION & BEAUTY
+        </h1>
+        <p className="text-[#9C9C9C] md:pt-5 pl-5 md:text-2xl">
+        Elevate Your Style with Expert Stylists, Makeup Artists, and Skincare Specialists
+        </p>
+      </div>
 
         {/* Filter Button */}
         <div className="relative">
